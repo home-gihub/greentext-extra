872714ee76ea697b1246fa472623d0e7c570c753
home-github <124263428+home-gihub@users.noreply.github.com>
2023-10-21 08:29:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+out = document.getElementById("outputf")
+inp = document.getElementById("inputf")
+btn = document.getElementById("abtn")
+
+
+function f() {
+var lines = inp.value.split('\n');    // lines is an array of strings
+out.innerHTML = null
+for (var j = 0; j < lines.length; j++) {
+    console.log("line complete")
+    if (lines[j] == "---") {
+        decode = `<hr>`
+    } else {
+        firstLetter = lines[j].substring(0, 1)
+        if ( firstLetter == ">") {
+            decode = `<p class="green">${lines[j]}</p>`
+        } else if ( firstLetter == "|") {
+            decode = `<p class="gray">${lines[j]}</p>`
+        } else if ( firstLetter == "^") {
+            decode = `<p class="purple">${lines[j]}</p>`
+        } else if ( firstLetter == "!") {
+            decode = `<p class="yellow">${lines[j]}</p>`
+        } else if ( firstLetter == "-") {
+            decode = `<p class="orange">•${lines[j].substring(1,lines[j].length)}</p>`
+        } else if ( firstLetter == "?") {
+            decode = `<p class="blue">${lines[j]}</p>`
+        } else {
+        decode = `<p>${lines[j]}</p>`
+        }
+    }
+  out.innerHTML = `${out.innerHTML}
+  ${decode}
+  `
+}}
+
+btn.addEventListener("click", f)
